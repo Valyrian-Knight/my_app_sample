27de4158b0e3bd000195181ea5ddc5caf119e859
Robert Jonjic <robert.jonjic@gmail.com>
2019-02-21 16:12:04 +0000

    another change

---
+++ test_code.py
@@ -0,0 +1,53 @@
+# str_1 = 'The quick brown fox jumps over the lazy dog'
+# list_rev = str_1.split()
+# for i in list_rev:
+#     print(i, end = ' ')
+
+# Python 3 code for printing
+# on the same line printing
+# geeks and geeksforgeeks
+# in the same line
+
+# print("geeks", end =" ")
+# print("geeksforgeeks")
+
+# # array
+# #a = [1,2,3,4,5,6]
+
+# # printing a element in same
+# # line
+# for i in range(1, 11):
+#     print(i, end =" ")
+
+# Python program to print list
+# without using loop
+
+# a = [1, 2, 3, 4, 5]
+
+# # printing the list using * operator separated
+# # by space
+# print(*a)
+
+# # printing the list using * and sep operator
+# print("printing lists separated by commas")
+
+# print(*a, sep=", ")
+
+# # print in new line
+# print("printing lists in new line")
+
+# print(*a, sep = '\n')
+
+# Program to check input
+# type in Python
+
+num = int(input("Enter number :"))
+print(num)
+name1 = input("Enter name : ")
+print(name1)
+
+# Printing type of input value
+print("type of number", type(num))
+print("type of name", type(name1))
+
+print(str(num) + ' ' + name1)

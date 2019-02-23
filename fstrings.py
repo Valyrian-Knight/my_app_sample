b3627998634aef71515221a0924ba7d6e654d28d
Robert Jonjic <robert.jonjic@gmail.com>
2019-02-23 11:59:43 +0000

    fstrings testing

---
+++ fstrings.py
@@ -0,0 +1,11 @@
+first_name = 'Robert'
+last_name = 'Jonjic'
+age = 22
+
+# sentence = 'My name is {} {}'.format(first_name, last_name)
+# print(sentence)
+
+sentence = f'My name is {first_name} {last_name}. I am {age} years old!'
+print(sentence)
+
+print(type(age))

1fc3aaec6caafc28ac54b5a089e91188792f0cab
Robert Jonjic <robert.jonjic@gmail.com>
2019-02-23 12:55:23 +0000

    date formatting

--- fstrings.py
+++ fstrings.py
@@ -19,3 +19,17 @@
 # calculation = f'4 times 11 is equal to {4 * 11}'
 # print(calculation)
 
+# for n in range(1, 11):
+#     sentence = f'The value is {n:04}'     # additional formatting
+#     print(sentence)
+
+# pi = 3.14159265
+# sentence = f'Pi is equal to {pi:.4f}'     # additional formatting
+# print(sentence)
+
+from datetime import datetime
+
+birthday = datetime(1990, 9, 22)
+
+sentence = f'Jenn has a birthday on {birthday:%B %d, %Y}'
+print(sentence)

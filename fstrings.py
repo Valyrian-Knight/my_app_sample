a53a68e36e131fa3322d4c859e2293677191dd82
Robert Jonjic <robert.jonjic@gmail.com>
2019-02-23 12:17:56 +0000

    playing with the formatting

--- fstrings.py
+++ fstrings.py
@@ -1,11 +1,21 @@
-first_name = 'Robert'
-last_name = 'Jonjic'
-age = 22
+# first_name = 'Robert'
+# last_name = 'Jonjic'
+# age = 22
 
 # sentence = 'My name is {} {}'.format(first_name, last_name)
 # print(sentence)
 
-sentence = f'My name is {first_name} {last_name}. I am {age} years old!'
-print(sentence)
+# sentence = f'My name is {first_name} {last_name}. I am {age} years old!'
+# print(sentence)
 
-print(type(age))
+# print(type(age))
+
+# sentence = 'My name is {} and I am {} years old.'.format(person['name'], person['age'])
+# print(sentence)
+
+# sentence = f"My name is {person['name']} and I am {person['age']} years old."
+# print(sentence)
+
+# calculation = f'4 times 11 is equal to {4 * 11}'
+# print(calculation)
+
